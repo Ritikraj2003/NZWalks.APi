7e4690540661350300091fe19b5f715725c1b568
ritikshow <ritikbaryons@gmail.com>
2025-04-16 23:49:22 +0530

    JWT Setup

--- NZWalks/Program.cs
+++ NZWalks/Program.cs
@@ -1,5 +1,8 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using NZWalks.API.Data;
 using NZWalks.API.Mappings;
 using NZWalks.API.Repositories;
@@ -18,6 +21,20 @@ builder.Services.AddScoped<IRegionRepository,ImpeRegionRepository>();
 builder.Services.AddScoped<IWalksRepository, ImpWalksRepository>();
 builder.Services.AddScoped<IDifficulty, ImpDifficulty>();
 builder.Services.AddAutoMapper(typeof(AutomapperProfile));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(Options =>
+    Options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audiance"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,6 +45,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
